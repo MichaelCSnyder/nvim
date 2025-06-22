fc4111212366ae666a059b0f541e5c71bf50e6b6
MichaelCSnyder <msnyder1992@gmail.com>
2025-06-22 15:56:57 -0400

    adding new keymap

--- lua/michael/core/keymaps.lua
+++ lua/michael/core/keymaps.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = " "
 vim.opt.startofline = true
 
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -13,9 +14,6 @@ vim.keymap.set({ "n", "v" }, "<leader>p", '"0p')
 -- clear search highlights
 vim.keymap.set("n", "<leader><leader>", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
-
 -- tab management
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 -- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
@@ -23,7 +21,7 @@ vim.keymap.set("n", "<C-k>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  g
 vim.keymap.set("n", "<C-j>", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<A-k>", "<cmd>+tabm<CR>", { desc = "Move tab to the right" }) --  go to previous tab
 vim.keymap.set("n", "<A-j>", "<cmd>-tabm<CR>", { desc = "Move tab to the left" }) --  go to previous tab
-vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+vim.keymap.set("n", "<leader>td", "<cmd>tabnew %<CR>", { desc = "Duplicate current buffer in new tab" }) --  move current buffer to new tab
 
 -- Search and replace word under cursor
 vim.keymap.set("n", "<leader>s", function()
@@ -41,3 +39,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		})
 	end,
 })
+
+vim.keymap.set("n", "<leader>du", function()
+	vim.cmd("windo diffupdate")
+	vim.cmd("windo redraw")
+end, { desc = "Fix diff view alignment" })
