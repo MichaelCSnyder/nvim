18bf0d83756385fdac2a119bfd68df3faf17255e
Michael Snyder <michael.snyder@rfsmart.com>
2025-08-20 11:51:16 -0400

    Fixing comment and indenting

--- lua/michael/plugins/auto-session.lua
+++ lua/michael/plugins/auto-session.lua
@@ -1,17 +1,17 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    local auto_session = require("auto-session")
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
 
-    auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" }, -- directories you want to ignore
-    })
+		auto_session.setup({
+			auto_restore_enabled = false,
+			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" }, -- directories you want to ignore
+		})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
-    keymap.set("n", "<leader>wa", "<cmd>SessionSearch<CR>", { desc = "Search all sessions" }) -- restore last workspace session for current directory
-  end,
+		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+		keymap.set("n", "<leader>wa", "<cmd>SessionSearch<CR>", { desc = "Search all sessions" }) -- search all sessions
+	end,
 }
