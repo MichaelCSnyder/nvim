0657e44ec0820d707beda614075abd2b63810be7
Michael Snyder <michael.snyder@rfsmart.com>
2026-06-09 16:40:04 -0400

    Transitioning to snacks for most basic picker functionality

--- lua/michael/plugins/snacks.lua
+++ lua/michael/plugins/snacks.lua
@@ -1,33 +1,94 @@
 return {
 	"folke/snacks.nvim",
-	opts = {
-		bigfile = { enabled = false },
-		notifier = { enabled = false },
-		quickfile = { enabled = false },
-		statuscolumn = { enabled = false },
-		words = { enabled = false },
-		styles = {},
+	config = function()
+		require("snacks").setup({
+			bigfile = { enabled = false },
+			notifier = { enabled = false },
+			quickfile = { enabled = false },
+			statuscolumn = { enabled = false },
+			words = { enabled = false },
+			styles = {},
 
-		picker = {
-			enabled = true,
-			win = {
-				input = {
-					keys = {
-						["<C-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
-						["<C-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+			picker = {
+				enabled = true,
+				win = {
+					input = {
+						keys = {
+							["<C-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+							["<C-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+							["<C-k>"] = { "list_up", mode = { "i", "n" } },
+							["<C-j>"] = { "list_down", mode = { "i", "n" } },
+							["<C-q>"] = { "qflist", mode = { "i", "n" } },
+						},
 					},
 				},
 			},
-		},
-	},
+		})
+
+		-- snacks.picker reads preview files with Lua's file:lines(), which
+		-- keeps the trailing \r on CRLF files (e.g. RF-SMART), rendering as
+		-- ^M. Strip CR at the single choke point all preview content flows
+		-- through, before highlighting is applied.
+		local ok_preview, Preview = pcall(require, "snacks.picker.core.preview")
+		if ok_preview and type(Preview.set_lines) == "function" and not Preview.__crlf_patched then
+			local orig_set_lines = Preview.set_lines
+			Preview.set_lines = function(self, lines, offset)
+				local cleaned = vim.split((table.concat(lines, "\n")):gsub("\r", ""), "\n", { plain = true })
+				return orig_set_lines(self, cleaned, offset)
+			end
+			Preview.__crlf_patched = true
+		end
+	end,
 	keys = {
-		-- {
-		-- 	"<leader>gB",
-		-- 	function()
-		-- 		Snacks.picker.git_branches()
-		-- 	end,
-		-- 	desc = "Git Branches",
-		-- },
+		-- File pickers
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files (frecency)",
+		},
+		{
+			"<leader>fr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent Files",
+		},
+
+		-- Search
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Live Grep (with rg args)",
+		},
+		{
+			"<leader>fc",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Grep Word Under Cursor",
+		},
+
+		-- LSP
+		{
+			"<leader>fs",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+			desc = "LSP Document Symbols",
+		},
+
+		-- Git
+		{
+			"<leader>gs",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status",
+		},
 		{
 			"<leader>gl",
 			function()
--- lua/michael/plugins/telescope.lua
+++ lua/michael/plugins/telescope.lua
@@ -7,17 +7,11 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"sharkdp/fd",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		{
-			"nvim-telescope/telescope-frecency.nvim",
-			version = "*",
-		},
 		-- "folke/todo-comments.nvim",
-		"nvim-telescope/telescope-live-grep-args.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		local builtin = require("telescope.builtin")
 
 		telescope.setup({
 			defaults = {
@@ -50,47 +44,9 @@ return {
 					override_file_sorter = true, -- override the file sorter
 					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 				},
-				frecency = {
-					enable_prompt_mappings = true,
-					matcher = "default", -- options: "fuzzy" | "default"
-					show_scores = false,
-					show_unindexed = true, -- Shows files not yet in the database
-					db_root = vim.fn.stdpath("data"), -- Explicitly set database location
-					workspaces = {
-						--  utilize with syntax: ":code:"
-						--  alternatively, ":<tab>" to open list
-						["code"] = vim.fn.expand("~/Code/RF-SMART/netsuite"),
-						["unit"] = vim.fn.expand("~/Code/RF-SMART/test/unit"),
-						["int"] = vim.fn.expand("~/Code/RF-SMART/test/integration"),
-					},
-				},
 			},
 		})
 
 		telescope.load_extension("fzf") -- fuzzy finder sorting algorithm that replace default lua sorter
-		telescope.load_extension("frecency") -- applies frequency-based scoring (using internal frequency database) prior to handing off results to fzf
-		telescope.load_extension("live_grep_args") -- allows you to pass arguments (for file types, directories, etc.) to ripgrep during global searches
-
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		vim.keymap.set("n", "<leader>ff", function()
-			telescope.extensions.frecency.frecency({
-				workspace = "CWD",
-			})
-		end, { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		-- keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find grep string in cwd" })
-		-- run "Man rg" to see params to pass to live_grep
-		vim.keymap.set("n", "<leader>fg", function()
-			require("telescope").extensions.live_grep_args.live_grep_args()
-		end, { desc = "Find grep string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find document symbols" })
-		keymap.set("n", "<leader>fb", builtin.git_bcommits, { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Telescope: view git status for all files" })
-		keymap.set("n", "<leader>gc", builtin.git_bcommits, { desc = "Telescope: view all commits for current file" })
-		-- keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }
